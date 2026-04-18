aec177f92fbe7302b53f0839353c0816f205ba7e
Felipe Otelakoski <felipe@agenciasimetria.com.br>
2026-04-18 20:26:03 -0300

    feat: integrate Processes page with AppShell layout

--- src/pages/Processes.tsx
+++ src/pages/Processes.tsx
@@ -1,5 +1,13 @@
+import { AppShell } from "@/components/AppShell";
+import { RequireAuth } from "@/components/RequireAuth";
 import { ProcessRepositoryPanel } from "@/components/processes/ProcessRepositoryPanel";
 
 export default function Processes() {
-  return <ProcessRepositoryPanel />;
+  return (
+    <RequireAuth>
+      <AppShell>
+        <ProcessRepositoryPanel />
+      </AppShell>
+    </RequireAuth>
+  );
 }
