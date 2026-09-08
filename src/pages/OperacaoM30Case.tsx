94be162d46e59cb7eb48bb22f153ee8ae62609c3
Felipe Otelakoski <felipe@agenciasimetria.com.br>
2026-09-08 14:51:42 -0300

    fix: import useRef in OperacaoM30Case

--- src/pages/OperacaoM30Case.tsx
+++ src/pages/OperacaoM30Case.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState, useRef } from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useAcquireM30CaseLock } from "@/hooks/useM30CasePresence";
